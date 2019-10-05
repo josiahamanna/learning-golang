a6d513af932a32abe411727ed34639cd102ce105
josiahnissi <josiah@appinessworld.com>
2019-10-05 23:54:11 +0530

    Basics

---
+++ 1-Tutorial/echo-exercise-1.go
@@ -0,0 +1,11 @@
+// Exersise 1
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+}
---
+++ 1-Tutorial/echo-exercise-2.go
@@ -0,0 +1,13 @@
+// Echo exercise 2
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for index, arg := range os.Args[1:] {
+		fmt.Println(index, arg)
+	}
+}
